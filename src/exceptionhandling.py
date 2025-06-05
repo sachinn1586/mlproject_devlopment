70c486f3e51abf750313324a23c0d271054baddb
Sachin kumar <sachinkr1416@gmail.com>
2025-06-05 11:38:22 +0530

    Exception ERROR

--- src/exceptionhandling.py
+++ src/exceptionhandling.py
@@ -1,17 +1,18 @@
-
 import sys
 
 
-class customexception(Exception):
-    def __init__(self,error_message,error_details:sys):
-        self.error_message = error_message
-        _,_,exc_tb = error_details.exc_info()
+def error_message_details(error,error_detail:sys):
+    _,_,exc_tb=error_detail.exc_info()
+    filename = exc_tb.tb_frame.f_code.co_filename
+    error_message="Error occured in python script name [{0}]line number[{1}] line message[{2}]".format(
+    filename,exc_tb.tb_lineno.str(error)
+
+    )
+
+class customExceptiom(Exception):
+    def __init__(self,error_message,error_detail:sys):
+        super.__init__(error_message)
+        self.error_message=error_message_details(error_message,error_detail=error_detail)
         
-        self.lineno=exc_tb.tb_lineno
-        self.file_name=exc_tb.tb_frame.f_code.co_filename 
-    
     def __str__(self):
-        return "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
-        self.file_name, self.lineno, str(self.error_message))
-        
-        
+        return self.error_message
